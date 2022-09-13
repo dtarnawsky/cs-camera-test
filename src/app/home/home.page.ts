e6ddc12814ebde061e922771d04452d36833c25a
Damian Tarnawsky <damian@ionic.io>
2022-09-13 08:36:02 -0700

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, ImageOptions } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  cameraOptions: ImageOptions = {
+    resultType: CameraResultType.Base64,
+    source: CameraSource.Camera,
+    width: 1024,
+    quality: 50,
+  };
 
+  photoOptions: ImageOptions = {
+    resultType: CameraResultType.Base64,
+    source: CameraSource.Photos,
+    width: 1024,
+    quality: 50,
+  };
+
+  constructor() { }
+
+  async getCamera() {
+    const status = await Camera.checkPermissions();
+    console.log(status);
+
+    const photo = await Camera.getPhoto(this.cameraOptions);
+    console.log(photo);
+  }
+
+  async getGallery() {
+    const status = await Camera.checkPermissions();
+    console.log(status);
+
+    const photo = await Camera.getPhoto(this.photoOptions);
+    console.log(photo);
+  }
 }
